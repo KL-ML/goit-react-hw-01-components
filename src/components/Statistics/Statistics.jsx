9b5859efa806671339677cec65b737ac9cb468ea
Natalya Karpova <knataly0513@gmail.com>
2022-12-13 21:15:50 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,28 +1,37 @@
 import PropTypes from 'prop-types';
+import { Section, Title, List, Item, Label, Percentage } from './Statistics.styled';
 
 export const Statistics = ({ title, stats }) => {
     return (
-        <section>
+        <Section>
             {
                 title && (
-                    <h2>{title}</h2>
+                    <Title>{title}</Title>
                 )
             }
-            <ul>
+            <List>
                 {
                     stats.map(({ id, label, percentage }) => {
-                        return (<li key={id}>
-                            <span>{label}</span>
-                            <span>{percentage}</span>
-                        </li>)
+                        return (
+                            <Item key={id}>
+                                <Label>{label}</Label>
+                                <Percentage>{percentage}%</Percentage>
+                            </Item>
+                        )
                     })
                 }
-            </ul>
-        </section>
+            </List>
+        </Section>
     );
 };
 
 Statistics.propTypes = {
     title: PropTypes.string,
-    stats: PropTypes.array,
+    stats: PropTypes.arrayOf(
+        PropTypes.exact({
+            id: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired,
+            percentage: PropTypes.number.isRequired,
+        })
+    ),
 };
