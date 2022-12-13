1368b49db48bf7987240ab82ae141b5bf2e258ff
Natalya Karpova <knataly0513@gmail.com>
2022-12-13 21:15:46 +0200

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,30 +1,32 @@
 import PropTypes from 'prop-types';
+import css from './Profile.module.css';
 
 export const Profile = ({username, tag, location, avatar, stats}) => {
     return (
-        <div>
-            <div>
+        <div className={css.profile}>
+            <div className={css.description}>
                 <img
                     src={avatar}
                     alt={username}
+                    className={css.avatar}
                 />
-                <p>{username}</p>
-                <p>@{tag}</p>
-                <p>{location}</p>
+                <p className={css.name}>{username}</p>
+                <p className={css.tag}>@{tag}</p>
+                <p className={css.location}>{location}</p>
             </div>
 
-            <ul>
+            <ul className={css.stats}>
                 <li>
-                    <span>Followers</span>
-                    <span>{stats.followers}</span>
+                    <span className={css.label}>Followers</span>
+                    <span className={css.quantity}>{stats.followers}</span>
                 </li>
                 <li>
-                    <span>Views</span>
-                    <span>{stats.views}</span>
+                    <span className={css.label}>Views</span>
+                    <span className={css.quantity}>{stats.views}</span>
                 </li>
                 <li>
-                    <span>Likes</span>
-                    <span>{stats.likes}</span>
+                    <span className={css.label}>Likes</span>
+                    <span className={css.quantity}>{stats.likes}</span>
                 </li>
             </ul>
         </div>
