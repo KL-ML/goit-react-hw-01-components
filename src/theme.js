688f8327e030c96e16729a6c989d85cfa64df966
Natalya Karpova <knataly0513@gmail.com>
2022-12-15 16:37:41 +0200

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,44 @@
+export const theme = {
+    colors: {
+        text: `#000`,
+        background: 'gray',
+        primery: 'grey',
+        secondary: 'pink',
+        accent: 'orange',
+        muted: 'white',
+    },
+    space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+    fonts: {
+        body: 'system-ui, sans-serif',
+        heading: 'system-ui, sans-serif',
+        monospace: 'Menlo, monospace',
+    },
+    fontSizes: {
+        xs: "12px",
+        s: "14px",
+        m: "16px",
+        l: "32px",
+        xl: "64px",
+    },
+    fontWeights: {
+        normal: 400,
+        bold: 700,
+    },
+    lineHeights: {
+        body: 1.5,
+        heading: 1.125,
+    },
+    borders: {
+        none: 'none',
+        normal: '1px solid',
+    },
+    radii: {
+        none: '0',
+        normal: '4px',
+        round: '50%',
+    },
+    shadows: {
+        boxShadowSection: 'rgba(0, 0, 0, 0.16) 0px 1px 4px',
+        shadowStats: 'rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;',
+    }
+};
