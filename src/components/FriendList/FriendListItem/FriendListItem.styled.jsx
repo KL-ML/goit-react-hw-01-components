57173b4262ca668360492fae9fccc949055af8a1
Natalya Karpova <knataly0513@gmail.com>
2022-12-18 23:35:40 +0200

    Update FriendListItem.styled.jsx

--- src/components/FriendList/FriendListItem/FriendListItem.styled.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.styled.jsx
@@ -1,24 +1,27 @@
 import styled from "@emotion/styled";
 
-export const FriendsItem = styled.li`
-    display: flex;
-    align-items: center;
-    width: 250px;
-    border: solid 1px black;
-    border-radius: 4px;
-    margin-bottom: 10px;
-    padding: 3px 8px;
+export const IsOnlineDot = styled.div`
+    border-radius: ${p => p.theme.radii.round};
+    width: 12px;
+    height: 12px;
+    margin-right: ${p => p.theme.space[4]}px;
+    background-color: ${p => {
+        return p.isOnline
+            ? p.theme.colors.isOnline
+            : p.theme.colors.isOffline
+    }};
+    
 `;
 
-export const FriendIsOnline = styled.span`
-    margin-right: 5px;
-`;
 export const FriendAvatar = styled.img`
-    height: 48px;
-    width: 48px;
-    margin-right: 5px;
+    height: ${p => p.theme.sizes.friendsImgSize};
+    width: ${p => p.theme.sizes.friendsImgSize};
+    margin-right: ${p => p.theme.space[4]}px;
+    border-radius: ${p => p.theme.radii.normal};
+    background-color: ${p => p.theme.colors.text};
+    box-shadow: ${p => p.theme.shadows.shadowStats};
 `;
 
 export const FriendName = styled.p`
-    font-size: 18px;
+    font-size: ${p => p.theme.fontSizes[3]}px;
 `;
