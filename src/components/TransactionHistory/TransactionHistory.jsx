e61b68ce4a9a5ab63dade0349eef7e5f294d55c1
Natalya Karpova <knataly0513@gmail.com>
2022-12-18 23:35:51 +0200

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,33 +1,50 @@
 import PropTypes from 'prop-types';
+import { Table, Thead, Tbody, TableTH, TableTD } from './TransactionHistory.styled';
+import { Box } from 'components/Box';
 
 export const TransactionHistory = ({ items }) => {
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Type</th>
-                    <th>Amount</th>
-                    <th>Currency</th>
-                </tr>
-            </thead>
+        <Box
+            bg="backgroundCard"
+            boxShadow="boxShadowSection"
+            borderRadius="normal"
+            overflow="hidden"
+            
+        >
+            <Table>
+                <Thead>
+                    <tr>
+                        <TableTH>Type</TableTH>
+                        <TableTH>Amount</TableTH>
+                        <TableTH>Currency</TableTH>
+                    </tr>
+                </Thead>
 
-            <tbody>
-                {
-                    items.map(({ id, type, amount, currency }) => {
-                        return (
-                            <tr key={id}>
-                                <td>{type}</td>
-                                <td>{amount}</td>
-                                <td>{currency}</td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-        </table>
+                <Tbody>
+                    {
+                        items.map(({ id, type, amount, currency }) => {
+                            return (
+                                <tr key={id}>
+                                    <TableTD>{type}</TableTD>
+                                    <TableTD>{amount}</TableTD>
+                                    <TableTD>{currency}</TableTD>
+                                </tr>
+                            )
+                        })
+                    }
+                </Tbody>
+            </Table>
+        </Box>
     );
 };
 
 TransactionHistory.propTypes = {
-    items: PropTypes.array,
+    items: PropTypes.arrayOf(
+        PropTypes.exact({
+            id: PropTypes.string.isRequired,
+            type: PropTypes.string.isRequired,
+            amount: PropTypes.number.isRequired,
+            currency: PropTypes.string.isRequired,
+        })
+    )
 };
