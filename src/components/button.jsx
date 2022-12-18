ba95b98f16c5daca6824e8d91fb2d881d4d470ac
Natalya Karpova <knataly0513@gmail.com>
2022-12-18 23:35:34 +0200

    Update button.jsx

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -5,11 +5,11 @@ const StyledButton = styled.button`
     display: inline-flex;
     align-items: center;
     gap: ${p => p.theme.space[2]}px;
-  margin: ${p => p.theme.space[0]};
-  padding: ${p => p.theme.space[3]}px;
-  border: ${p => p.theme.borders.normal};
-  border-radius: ${p => p.theme.radii.normal};
-  background-color: ${p => {
+    margin: ${p => p.theme.space[0]};
+    padding: ${p => p.theme.space[3]}px;
+    border: ${p => p.theme.borders.normal};
+    border-radius: ${p => p.theme.radii.normal};
+    background-color: ${p => {
         return p.disabled
             ? p.theme.colors.secondary
             : p.theme.colors.primery
