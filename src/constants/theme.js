68ee1eaf4c66404c047dd68acdcceca52c7dd73d
Natalya Karpova <knataly0513@gmail.com>
2022-12-18 23:35:59 +0200

    Create theme.js

---
+++ src/constants/theme.js
@@ -0,0 +1,53 @@
+export const theme = {
+    colors: {
+        text: `#E4E2E5`,
+        background: '#2c3037',
+        backgroundCard: '#3A4047',
+        darkPink: '#CA68A3',
+        darkPerpel: '#B25CCE',
+        lightPink: '#C49EC7',
+        lightPerpel: '#A586D8',
+        accent: '#DE7B7B',
+        muted: '#948993',
+        isOnline: '#7ace85',
+        isOffline: '#DE7B7B'
+    },
+    space: [0, 2, 4, 8, 16, 24, 32, 64, 128, 256],
+    fonts: {
+        body: 'Roboto, sans-serif',
+        heading: 'Roboto, sans-serif',
+        monospace: 'Menlo, monospace',
+    },
+    fontSizes: [
+    12, 14, 16, 20, 24, 32, 48, 64,
+    ],
+    fontWeights: {
+        normal: 400,
+        bold: 700,
+    },
+    lineHeights: {
+        body: 1.5,
+        heading: 1.125,
+    },
+    borders: {
+        none: 'none',
+        normal: '1px solid',
+        bold: '2px solid'
+    },
+    radii: {
+        none: '0',
+        normal: '4px',
+        round: '50%',
+    },
+    shadows: {
+        containerShadow: 'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset',
+        boxShadowSection: 'rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;',
+        shadowStats: 'rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;',
+        avatarShadow: 'rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;',
+    },
+    sizes: {
+        containerWidth: '340px',
+        friendsImgSize: '58px',
+        tableHeadHeight: '40px',
+    }
+};
